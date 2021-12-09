12fc2397f204c85d0685817d2a24d6bce3ffa45d
roxdavirox <roxdavirox@gmail.com>
2021-12-09 09:23:19 -0300

    feat: get history service riot

--- packages/server/services/riot.service.js
+++ packages/server/services/riot.service.js
@@ -1,43 +1,16 @@
 "use strict";
-const HTTPClientService = require("moleculer-http-client");
 
-/**
- * @typedef {import('moleculer').Context} Context Moleculer's Context
- */
+const HTTPClientService = require("moleculer-http-client");
 
 module.exports = {
 	name: "riot",
-
 	mixins: [HTTPClientService],
-	/**
-	 * Settings
-	 */
-
-
 	settings: {
-		// HTTP client settings
 		httpClient: {
-			// Boolean value indicating whether request should be logged or not
 			logging: true,
-
-			// Format the Response
 			responseFormatter: "body", // one of "body", "headers", "status", "raw"
-
-
-			// Got Client options
-			defaultOptions: {
-				// Put here any Got available option that can be used to extend Got client
-			}
 		}
 	},
-	/**
-	 * Dependencies
-	 */
-	dependencies: [],
-
-	/**
-	 * Actions
-	 */
 	actions: {
 
 		/**
@@ -51,47 +24,59 @@ module.exports = {
 			},
 			/** @param {Context} ctx  */
 			async handler(ctx) {
-				const response = await ctx.call("riot.get", {
-					url: `https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${ctx.params.name}?api_key=RGAPI-536c4675-861e-46bb-8027-66ce5223206f`,
-					opt: { responseType: "json" }
-				});
-				return response;
+				const { puuid } =  await this.getSummonerByName(ctx);
+				const history = await this.getSummonerMatchHistory(ctx, puuid);
+				const historyRequests = history.map(async matchId => await this.getHistoryByMatchId(ctx, matchId));
+				const matches = await Promise.all(historyRequests);
+				
+				return this.filterParticipantById(puuid, matches);
 			}
 		}
 	},
 
-	/**
-	 * Events
-	 */
-	events: {
-
-	},
-
-	/**
-	 * Methods
-	 */
 	methods: {
+		async getSummonerByName(ctx) {
+			const response = await ctx.call("riot.get", {
+				url: `https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${ctx.params.name}?api_key=RGAPI-536c4675-861e-46bb-8027-66ce5223206f`,
+				opt: { responseType: "json" }
+			});
+			return response;
+		},
 
+		async getSummonerMatchHistory(ctx, puuid) {
+			const response = await ctx.call("riot.get", {
+				url: `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=5&api_key=RGAPI-536c4675-861e-46bb-8027-66ce5223206f`,
+				opt: { responseType: "json" }
+			});
+			return response;
+		},
+
+		async getHistoryByMatchId(ctx, matchId) {
+			const response = await ctx.call("riot.get", {
+				url: `https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=RGAPI-536c4675-861e-46bb-8027-66ce5223206f`,
+				opt: { responseType: "json" }
+			});
+			const { info } = response;
+			return info;
+		},
+
+		filterParticipantById(puuid, matches) {
+			return matches.map(({ participants }) => participants.filter(({ puuid: id }) => id == puuid))
+				.map(([{ 
+					kills,
+					deaths,
+					assists,
+					win,
+					championName,
+					totalMinionsKilled
+				}]) => ({ 
+					kills,
+					deaths,
+					assists,
+					win,
+					championName,
+					totalMinionsKilled
+				}));
+		}
 	},
-
-	/**
-	 * Service created lifecycle event handler
-	 */
-	created() {
-
-	},
-
-	/**
-	 * Service started lifecycle event handler
-	 */
-	async started() {
-
-	},
-
-	/**
-	 * Service stopped lifecycle event handler
-	 */
-	async stopped() {
-
-	}
 };
