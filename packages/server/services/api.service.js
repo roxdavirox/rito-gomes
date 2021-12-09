6336556572b31908b26ebc0ab08928a16a24a0bc
roxdavirox <roxdavirox@gmail.com>
2021-12-09 04:35:28 -0300

    chore: enable cors

--- packages/server/services/api.service.js
+++ packages/server/services/api.service.js
@@ -20,6 +20,21 @@ module.exports = {
 		// Exposed IP
 		ip: "0.0.0.0",
 
+		cors: {
+			// Configures the Access-Control-Allow-Origin CORS header.
+			origin: "*",
+			// Configures the Access-Control-Allow-Methods CORS header. 
+			methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+			// Configures the Access-Control-Allow-Headers CORS header.
+			allowedHeaders: [],
+			// Configures the Access-Control-Expose-Headers CORS header.
+			exposedHeaders: [],
+			// Configures the Access-Control-Allow-Credentials CORS header.
+			credentials: false,
+			// Configures the Access-Control-Max-Age CORS header.
+			maxAge: 3600
+		},
+
 		// Global Express middlewares. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Middlewares
 		use: [],
 
